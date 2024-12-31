e8f57ef5a8a5637e73922653a870310f3ab73785
reju-1 <rahmed1445@gmail.com>
2024-12-31 18:25:01 +0600

    Refactoring

--- LeetCode/1.Arrays-&-Hashing/m.group_anagrams.py
+++ LeetCode/1.Arrays-&-Hashing/m.group_anagrams.py
@@ -3,12 +3,20 @@
 link: https://leetcode.com/problems/group-anagrams/description/
 """
 
+from collections import defaultdict
+
 
 class Solution:
-    def groupAnagrams(self, strs: list[str]) -> list[list[str]]: 
-        ...
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        anagrams = defaultdict(list)
 
-    def solve_by_sorting(self, strs: list[str]) -> list[list[str]]:
+        for string in strs:
+            sorted_key = "".join(sorted(string))
+            anagrams[sorted_key].append(string)
+
+        return list(anagrams.values())
+
+    def first_solution(self, strs: list[str]) -> list[list[str]]:
 
         hash_map: dict[str, list[str]] = {}
 

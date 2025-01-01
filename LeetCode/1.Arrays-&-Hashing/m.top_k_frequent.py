24c24b471bc1c30920e291a3369422724163abfd
reju-1 <rahmed1445@gmail.com>
2025-01-01 18:19:52 +0600

    lc-347: Top K Frequent Elements

---
+++ LeetCode/1.Arrays-&-Hashing/m.top_k_frequent.py
@@ -0,0 +1,36 @@
+""" 
+347. Top K Frequent Elements
+link: https://leetcode.com/problems/top-k-frequent-elements/description/
+"""
+
+from collections import Counter
+
+
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        """
+        Time O(n)
+        Space O(n)
+
+        Related to Bucket sort or Counting Sort.
+
+        Other possible ways:
+            - Counter(nums).most_common(k)
+            - by Sorting n*lgo(n)
+            - by using Heap n*log(k)
+        """
+        counter = Counter(nums)
+
+        buckets = [[] for _ in range(len(nums) + 1)]
+
+        for number, frequency in counter.items():
+            buckets[frequency].append(number)
+
+        results = []
+        for bucket in buckets[::-1]:
+            for num in bucket:
+                results.append(num)
+                k -= 1
+
+                if k == 0:
+                    return results

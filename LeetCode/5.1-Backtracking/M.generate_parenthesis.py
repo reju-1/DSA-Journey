9157413dabb9ad2fac04018f760832c37d4dc404
reju-1 <rahmed1445@gmail.com>
2025-02-01 23:04:24 +0600

    rename

--- LeetCode/5.1-Backtracking/M.generate_parenthesis.py
+++ LeetCode/5.1-Backtracking/M.generate_parenthesis.py
@@ -8,16 +8,16 @@ class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
         result = []
 
-        def helper(parenthesis: str, open_count: int, close_count: int):
+        def dfs(s: str, open_count: int, close_count: int):
             if n == open_count == close_count:
-                result.append(parenthesis)
+                result.append(s)
                 return
 
             if open_count < n:
-                helper(parenthesis + "(", open_count + 1, close_count)
+                dfs(s + "(", open_count + 1, close_count)
             if close_count < open_count:
-                helper(parenthesis + ")", open_count, close_count + 1)
+                dfs(s + ")", open_count, close_count + 1)
 
-        helper("", 0, 0)
+        dfs("", 0, 0)
 
         return result

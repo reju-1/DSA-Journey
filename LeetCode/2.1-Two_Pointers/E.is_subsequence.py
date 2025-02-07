44b4ea9fd218696d5bbc4f32a83172f6b225c808
reju-1 <rahmed1445@gmail.com>
2025-02-07 12:12:58 +0600

    added recursive solution

--- LeetCode/2.1-Two_Pointers/E.is_subsequence.py
+++ LeetCode/2.1-Two_Pointers/E.is_subsequence.py
@@ -28,3 +28,19 @@ class Solution:
             tp += 1
 
         return sp == len(s)
+
+    def isSubsequence(self, s: str, t: str) -> bool:
+        """with linier recursion."""
+
+        def helper(sp: int, tp: int):
+            if sp == len(s):
+                return True
+            if tp == len(t):
+                return False
+
+            if s[sp] == t[tp]:
+                return helper(sp + 1, tp + 1)
+            else:
+                return helper(sp, tp + 1)
+
+        return helper(sp=0, tp=0)

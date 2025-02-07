276af6e6ac91bf5583df205dedfa6e64d85f1e0e
reju-1 <rahmed1445@gmail.com>
2025-02-07 11:41:47 +0600

    Lc: is sub-sequence

---
+++ LeetCode/2.1-Two_Pointers/E.is_subsequence.py
@@ -0,0 +1,30 @@
+"""
+392. Is Subsequence
+link: https://leetcode.com/problems/is-subsequence/
+"""
+
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        """Single pointer"""
+
+        cursor = 0
+        for c in t:
+            if cursor == len(s):
+                return True
+
+            if c == s[cursor]:
+                cursor += 1
+
+        return cursor == len(s)
+
+    def isSubsequence(self, s: str, t: str) -> bool:
+        """Two pointer"""
+        sp, tp = 0, 0
+
+        while sp < len(s) and tp < len(t):
+            if s[sp] == t[tp]:
+                sp += 1
+            tp += 1
+
+        return sp == len(s)

e117f3deee22cfb8a92b57b7a53eac898c9d28a8
reju-1 <rahmed1445@gmail.com>
2025-02-26 10:14:30 +0600

    Lc: 3sum hashSet solution added

--- LeetCode/2.1-Two_Pointers/M.3sum.py
+++ LeetCode/2.1-Two_Pointers/M.3sum.py
@@ -1,3 +1,9 @@
+"""
+15. 3Sum
+Link: https://leetcode.com/problems/3sum/
+"""
+
+
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         """
@@ -37,3 +43,34 @@ class Solution:
                     high -= 1
 
         return answer
+
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        """
+        Time: O(n^2)
+        Space: O(n)
+        Remarks: HashSet Solution
+        """
+
+        nums.sort()
+        answer = set()
+
+        for i, val in enumerate(nums):
+
+            if val > 0:  # Optimization
+                break
+
+            left, right = i + 1, len(nums) - 1
+
+            while left < right:
+                sum3 = val + nums[left] + nums[right]
+                if sum3 == 0:
+                    answer.add((val, nums[left], nums[right]))
+                    left += 1
+                    right -= 1
+
+                elif sum3 < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return [list(x) for x in answer]

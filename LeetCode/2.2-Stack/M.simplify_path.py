6a9bac54a49a055df79f493f741fc1ef5a3a7203
reju-1 <rahmed1445@gmail.com>
2025-02-28 01:12:19 +0600

    New solution Lc: simplify path

--- LeetCode/2.2-Stack/M.simplify_path.py
+++ LeetCode/2.2-Stack/M.simplify_path.py
@@ -21,6 +21,29 @@ class Solution:
 
         return "/" + "/".join(directories)
 
+    def simplifyPath(self, path: str) -> str:
+        directories = []
+        i, n = 0, len(path)
+
+        while i < n:
+            while i < n and path[i] == "/":
+                i += 1  # Skipping slashes
+
+            start = i
+            while i < n and path[i] != "/":
+                i += 1  # Finding end of directory
+
+            new_dir = path[start:i]
+
+            if new_dir == "..":
+                if directories:  # Not []
+                    directories.pop()  # Moving up one level
+
+            elif new_dir and new_dir != ".":  # not empty and "."
+                directories.append(new_dir)
+
+        return "/" + "/".join(directories)
+
 
 unix_path = Solution().simplifyPath("/...///a/../b/c/../d/./")
 print(f"Absolute path: {unix_path}")

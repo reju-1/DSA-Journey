f9860161a0468c430407b71ad34d6814f2bdab53
reju-1 <rahmed1445@gmail.com>
2025-03-10 22:51:23 +0600

    Lc: 3 -> New hash map solution added

--- LeetCode/3.2-Sliding_Window/M.longest_substring_not_repeating_char.py
+++ LeetCode/3.2-Sliding_Window/M.longest_substring_not_repeating_char.py
@@ -15,13 +15,28 @@ class Solution:
         left = count = 0
 
         for right, char in enumerate(s):
-
-            while char in hash_set:  # remove upto duplicate character
+            # Remove characters from the left until the duplicate is removed
+            while char in hash_set:
                 hash_set.remove(s[left])
                 left += 1
 
             hash_set.add(char)
             # count = max(count, len(hash_set))
             count = max(count, right - left + 1)
+
+        return count
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        hash_map = {}  # {char: index}
+        left = count = 0
+
+        for right, char in enumerate(s):
+            # If char is in the map and within the current window
+            if char in hash_map and hash_map[char] >= left:
+                left = hash_map[char] + 1  # left = prev_occurrence + 1
+
+            count = max(count, right - left + 1)
+            hash_map[char] = right  # Update last seen index
 
         return count

2bd4fd6dbd90673f9c82db6c9f56ce6a3fa219c4
reju-1 <rahmed1445@gmail.com>
2025-03-13 15:49:27 +0600

    Lc: 42 -> New solution added

--- LeetCode/2.1-Two_Pointers/H.trapping_rain_water.py
+++ LeetCode/2.1-Two_Pointers/H.trapping_rain_water.py
@@ -33,5 +33,28 @@ class Solution:
 
         return result
 
+    def trap(self, height: list[int]) -> int:
+        """
+        Two pointer technique
+        Time: O(n)
+        Space: O(1)
+        """
+
+        res = 0
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]  # Think why the water volume always positive
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+
+        return res
+
 
 Solution().trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])

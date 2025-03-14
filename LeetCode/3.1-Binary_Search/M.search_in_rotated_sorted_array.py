36351a2d4350a968e66a511d5da309593ff1b575
reju-1 <rahmed1445@gmail.com>
2025-03-14 16:11:01 +0600

    Lc: 33. Search in Rotated Sorted Array

---
+++ LeetCode/3.1-Binary_Search/M.search_in_rotated_sorted_array.py
@@ -0,0 +1,40 @@
+"""
+33. Search in Rotated Sorted Array
+https://leetcode.com/problems/search-in-rotated-sorted-array/
+"""
+
+
+class Solution:
+    def _search(self, nums: list[int], target: int, l: int, r: int) -> int:
+
+        if l > r:
+            return -1
+
+        mid = (l + r) // 2
+        if nums[mid] == target:
+            return mid
+
+        if nums[l] <= nums[mid]:  # Left half is sorted
+
+            # target is within sorted array ?
+            if nums[l] <= target <= nums[mid]:
+                return self._search(nums, target, l, mid - 1)
+            else:  # Otherwise target must be in right half of array
+                return self._search(nums, target, mid + 1, r)
+
+        else:
+            # right is sorted and target is within this half of array?
+            if nums[mid] <= target <= nums[r]:
+                return self._search(nums, target, mid + 1, r)
+            else:  # Otherwise target must be in left half of array
+                return self._search(nums, target, l, mid - 1)
+
+    def search(self, nums: list[int], target: int) -> int:
+        """
+        The key idea is that in a rotated sorted array, at least one half (left or right) is always sorted.
+        We use this property to decide which half to search in, narrowing down the search space efficiently.
+        Time: O(log n)
+        Space: O(log n) [Recursion Stack]
+        """
+
+        return self._search(nums, target, 0, len(nums) - 1)

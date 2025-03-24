23b1218ee5374d0b62fc949dacdb87bebfacdd6d
reju-1 <rahmed1445@gmail.com>
2025-03-24 12:22:23 +0600

    Lc: 100. Same Tree

---
+++ LeetCode/4-Trees/E.same_tree.py
@@ -0,0 +1,27 @@
+"""
+100. Same Tree
+link: https://leetcode.com/problems/same-tree/
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: TreeNode | None, q: TreeNode | None) -> bool:
+        if p is None and q is None:
+            return True
+        elif p is None or q is None:  # Either one of is None
+            return False
+
+        # Same tree if the values and left and right sub-tree are equal
+        return (
+            p.val == q.val
+            and self.isSameTree(p.left, q.left)
+            and self.isSameTree(p.right, q.right)
+        )

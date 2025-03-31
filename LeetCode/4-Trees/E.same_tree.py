8bfc3627e164a9e5288256cce2ea6d3b6b224607
reju-1 <rahmed1445@gmail.com>
2025-03-31 15:26:08 +0600

    Lc: 100 -> New Solution Added

--- LeetCode/4-Trees/E.same_tree.py
+++ LeetCode/4-Trees/E.same_tree.py
@@ -15,8 +15,8 @@ class TreeNode:
 class Solution:
     def isSameTree(self, p: TreeNode | None, q: TreeNode | None) -> bool:
         """
-        Time: O(n)
-        Space: O(h)
+        Time: O(n) or O(n+m)
+        Space: O(h) or O(h_p + h_q)
         Remarks:
             - DFS approach
         """
@@ -32,3 +32,33 @@ class Solution:
             and self.isSameTree(p.left, q.left)
             and self.isSameTree(p.right, q.right)
         )
+
+    def isSameTreeV2(self, p: TreeNode | None, q: TreeNode | None) -> bool:
+        """
+        Time: O(n)
+        Space: O(n)
+        Remarks:
+            - Modified level-order traversal with `None` marker for Tracking structural integrity
+        """
+
+        queue = [p, q]
+
+        while queue:
+            node1 = queue.pop(0)
+            node2 = queue.pop(0)
+
+            if not node1 and not node2:  # Both None, meaning identical structure
+                continue
+
+            # If only one of the node is None OR values differ, trees are not the same
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+
+            # left sub-tree with left sub-tree
+            queue.append(node1.left)
+            queue.append(node2.left)
+            # right sub-tree with right sub-tree
+            queue.append(node1.right)
+            queue.append(node2.right)
+
+        return True

1c393e54f909c83be926091757934ce2e33745e2
reju-1 <rahmed1445@gmail.com>
2025-04-01 00:15:16 +0600

    Lc: 199 -> New solution added

--- LeetCode/4-Trees/M.binary_tree_right_side_view.py
+++ LeetCode/4-Trees/M.binary_tree_right_side_view.py
@@ -42,3 +42,27 @@ class Solution:
                     queue.append(node.right)
 
         return answer
+
+    def rightSideView(self, root: TreeNode | None) -> list[int]:
+        """
+        Time & Space: Typical DFS complexity.
+        Remarks:
+            - Related problem LeetCode 102. Binary Tree Level Order Traversal (especially **DFS approach**).
+        """
+        answer: list[int] = []
+
+        def dfs(curr: TreeNode | None, depth):
+            if curr is None:
+                return
+
+            # When DFS reaches a new level for the first time, this block executes.
+            # It runs once per level, ensuring we capture the rightmost node first.
+            # We traverse the right subtree first, which allows us to take the right-side view of the tree.
+            if len(answer) == depth:
+                answer.append(curr.val)
+
+            dfs(curr.right, depth + 1)
+            dfs(curr.left, depth + 1)
+
+        dfs(root, 0)
+        return answer

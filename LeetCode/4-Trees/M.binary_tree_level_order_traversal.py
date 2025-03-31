1072f7a29df807c163e526cf111d2d1ee535b5c1
reju-1 <rahmed1445@gmail.com>
2025-03-31 23:28:37 +0600

    Lc: 102 -> New solution added

--- LeetCode/4-Trees/M.binary_tree_level_order_traversal.py
+++ LeetCode/4-Trees/M.binary_tree_level_order_traversal.py
@@ -43,3 +43,32 @@ class Solution:
             results.append(level_nodes)
 
         return results
+
+    def levelOrderV2(self, root: TreeNode | None) -> list[list[int]]:
+        """
+        Time: O(n)
+        Space: O(h)
+        Remarks:
+            DFS Pre-order traversal (node, left, right) that uses each node's depth
+            to place values in the correct level list, creating new lists as
+            needed when visiting deeper levels for the first time.
+        """
+        answer = []  # 2D list
+
+        def dfs(curr: TreeNode | None, depth):
+            if curr is None:
+                return
+
+            # Create a new level list when reaching a new depth for the first time
+            if len(answer) == depth:
+                answer.append([])
+
+            # Append the current node's value to it's level list
+            answer[depth].append(curr.val)
+
+            # Recursively traverse left and right subtrees
+            dfs(curr.left, depth + 1)
+            dfs(curr.right, depth + 1)
+
+        dfs(root, 0)
+        return answer

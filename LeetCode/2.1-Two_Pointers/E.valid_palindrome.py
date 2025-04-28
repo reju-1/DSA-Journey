25278370b5fb77a50ed10d862f6c2ff69c1ecb02
reju-1 <rahmed1445@gmail.com>
2025-04-28 17:44:49 +0600

    Add notes to Two Pointers section

--- LeetCode/2.1-Two_Pointers/E.valid_palindrome.py
+++ LeetCode/2.1-Two_Pointers/E.valid_palindrome.py
@@ -1,3 +1,9 @@
+"""
+125. Valid Palindrome
+link: https://leetcode.com/problems/valid-palindrome/
+"""
+
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         """
---
+++ LeetCode/2.1-Two_Pointers/README.md
@@ -0,0 +1,8 @@
+# Two Pointers
+
+## Important Problems
+
+- Valid Palindrome II (Easy)
+- 3Sum (Medium)
+- Container With Most Water (Medium)
+- Trapping Rain Water (Hard)

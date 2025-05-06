bedc7e4d032c7be6fb9889b9d4862d598f8ed85c
reju-1 <rahmed1445@gmail.com>
2025-05-06 19:56:36 +0600

    Lc: new solution added

--- LeetCode/1.1-Arrays_and_Strings/M.reverse_words_in_a_string.py
+++ LeetCode/1.1-Arrays_and_Strings/M.reverse_words_in_a_string.py
@@ -5,7 +5,7 @@ link: https://leetcode.com/problems/reverse-words-in-a-string/
 
 
 class Solution:
-    def reverseWords(self, s: str) -> str:
+    def reverseWordsV1(self, s: str) -> str:
         """
         Time: O(N)
         Space: O(N)
@@ -62,3 +62,52 @@ class Solution:
                 result.append(s[i + 1 : end + 1])
 
         return " ".join(result)
+
+    def reverseWordsV4(self, s: str) -> str:
+        """
+        Time: O(N)
+        Space: O(1)
+
+        Remarks:
+            - Follow-up: If strings are mutable in your language, can you solve it in-place using O(1) space?
+        Approach:
+            1. Convert the input string into a list of characters in "reversed order" to allow in-place operations.
+            2. Traverse the list, skip extra spaces, and shift valid words forward.
+            3. Reverse each individual word to restore their order.
+            4. Insert single spaces between words and remove any extra trailing space.
+        """
+
+        def reverse(left, right):
+            while left < right:
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+
+        s = list(s[::-1])
+        n = len(s)
+        i = j = 0  # i: read pointer, j: write pointer
+
+        while i < n:
+            # Skip spaces
+            while i < n and s[i] == " ":
+                i += 1
+
+            if i == n:
+                break  # End of string
+
+            # Capture the start of the new word
+            start = j
+            while i < n and s[i] != " ":
+                # Shifting characters forward
+                s[j] = s[i]
+                i += 1
+                j += 1
+
+            # Reverse the copied word to fix its order
+            reverse(start, j - 1)
+
+            if j < n:  # Insert a single space if there are more words
+                s[j] = " "
+                j += 1
+
+        return "".join(s[:j]).rstrip()  # Right striping probable space

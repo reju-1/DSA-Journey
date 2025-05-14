e08bca552b0188692964af341863232e7304e8fe
reju-1 <rahmed1445@gmail.com>
2025-05-14 21:56:23 +0600

    Refactoring Lc: 22

--- LeetCode/5.2-Backtracking/M.generate_parenthesis.py
+++ LeetCode/5.2-Backtracking/M.generate_parenthesis.py
@@ -1,11 +1,24 @@
+"""
+22. Generate Parentheses
+link: https://leetcode.com/problems/generate-parentheses/
+"""
+
+
 class Solution:
-    """
-    Time complexity: O(4^n / sqrt(n))
-
-    Todo: Learn the solution that solved with stack
-    """
-
     def generateParenthesis(self, n: int) -> list[str]:
+        """
+        Complexity:
+            links:
+                - https://www.youtube.com/watch?v=TAuJV5eNKLM
+                - https://en.wikipedia.org/wiki/Catalan_number#Applications_in_combinatorics
+                - https://www.youtube.com/watch?v=FcHyVFRd2fY
+                - https://www.youtube.com/watch?v=1ZAotQGnbi4
+
+            Time: O(4^n / sqrt(n))
+            Space:
+                - O(n) for the recursion call stack
+                - O(4^n / sqrt(n)) for storing the result list
+        """
         result = []
 
         def dfs(s: str, open_count: int, close_count: int):
@@ -21,3 +34,6 @@ class Solution:
         dfs("", 0, 0)
 
         return result
+
+
+# Todo: Learn the solution that solved with stack

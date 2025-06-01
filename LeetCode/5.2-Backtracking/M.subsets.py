f823579ddd0c0d71b00430c29e0f5765fa864ddd
reju-1 <rahmed1445@gmail.com>
2025-06-01 15:47:03 +0600

    New solution: Lc 78

--- LeetCode/5.2-Backtracking/M.subsets.py
+++ LeetCode/5.2-Backtracking/M.subsets.py
@@ -7,10 +7,13 @@ link: https://leetcode.com/problems/subsets/
 class Solution:
     def subsets(self, nums: list[int]) -> list[list[int]]:
         """
-        Time: O(n * 2^n)
-        Space: O(n) for recursion stack. O(2^n) for result.
+        Time: O(n * 2^n) — There are 2^n subsets, and each subset takes up to O(n) time to copy.
+        Space:
+            - O(n) for recursion stack.
+            - O(n * 2^n) for output — 2^n subsets, each up to size n.
         Remarks:
-            - Parameterized recursive backtracking approach.
+            - 2 Branch recursion (include/exclude)
+            - `Parameterized recursive` backtracking approach.
         """
         result = []
 
@@ -29,7 +32,8 @@ class Solution:
         """
         Time & Space: Same as previous.
         Remarks:
-            - Uses a single shared list + DFS.
+            - 2 Branch recursion (include/exclude)
+            - Memory-friendly backtracking with a single mutable `list object`.
         """
         result = []
         curr_subset = []
@@ -46,3 +50,57 @@ class Solution:
 
         dfs(0)
         return result
+
+    def subsetsV3(self, nums: list[int]) -> list[list[int]]:
+        """
+        Time & Space: Same as previous.
+        Remarks:
+            - 2 Branch recursion (include/exclude)
+            - purely `functional recursion` without globals. (No mutable state, No side effects)
+        """
+
+        def dfs(i: int, cur_set: list) -> list[list[int]]:
+            if i == len(nums):
+                return [cur_set[:]]
+
+            # return dfs(i + 1, cur_set) + dfs(i + 1, cur_set + [nums[i]])  # CONCISE
+
+            result = []
+            result += dfs(i + 1, cur_set)  # No Take
+            result += dfs(i + 1, cur_set + [nums[i]])  # Take
+            return result
+
+        return dfs(0, [])
+
+    def subsetsV4(self, nums: list[int]) -> list[list[int]]:
+        """
+        Time & Space: Same as previous.
+        Remarks:
+            - N-branch recursive backtracking (for-loop).
+            - Uses a shared (non-local) list to store result.
+        """
+        result = []
+
+        def dfs(start: int, subset: list):
+            result.append(subset[:])
+            for i in range(start, len(nums)):
+                dfs(i + 1, subset + [nums[i]])
+
+        dfs(0, [])
+        return result
+
+    def subsetsV5(self, nums: list[int]) -> list[list[int]]:
+        """
+        Time & Space: Same as previous.
+        Remarks:
+            - N-branch recursive backtracking (for-loop).
+            - Recursively constructs and aggregates subset results.
+        """
+
+        def dfs(start: int, subset: list) -> list[list]:
+            result = [subset[:]]
+            for i in range(start, len(nums)):  # No Out of bounds call due to range()
+                result += dfs(i + 1, subset + [nums[i]])
+            return result
+
+        return dfs(0, [])

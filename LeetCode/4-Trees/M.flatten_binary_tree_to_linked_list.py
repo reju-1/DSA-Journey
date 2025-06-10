662bd777f1aeefef1f8b7995a373c7bb7779c1d3
reju-1 <rahmed1445@gmail.com>
2025-06-11 01:00:59 +0600

    New solution added: Lc 114

--- LeetCode/4-Trees/M.flatten_binary_tree_to_linked_list.py
+++ LeetCode/4-Trees/M.flatten_binary_tree_to_linked_list.py
@@ -13,24 +13,50 @@ class TreeNode:
 
 
 class Solution:
+    """
+    Do not return anything, modify root in-place instead.
+    """
+
     def flatten(self, root: TreeNode | None) -> None:
         """
-        Do not return anything, modify root in-place instead.
-        Naive approach: find the PreOrder sequence and converted into linked list.
+        Time: O(N)
+        Space: O(H)
         """
         if root is None:
             return None
 
-        def preOrder(curr: TreeNode | None):
-            if curr is None:
-                return []
-            return [curr.val] + preOrder(curr.left) + preOrder(curr.right)
+        self.flatten(root.left)
 
-        sequence = preOrder(root)
+        cur_right = root.right
+        root.right = root.left
+        root.left = None
+        while root.right:
+            root = root.right
+        root.right = cur_right
 
-        temp = root
-        temp.left = None
+        self.flatten(root.right)
 
-        for num in sequence[1:]:
-            temp.right = TreeNode(num)
-            temp = temp.right
+    def flattenV2(self, root: TreeNode | None) -> None:
+        """
+        Time: O(N)
+        Space: O(1)
+        Remarks:
+            - Modified `Morris traversal` approach
+        link:
+            - https://leetcode.com/problems/flatten-binary-tree-to-linked-list/solutions/1207642/js-python-java-c-simple-o-1-space-recursive-solutions-w-explanation/
+        """
+        curr = root
+
+        while curr:
+            if curr.left:
+                # If the current node has a left subtree, find its rightmost node (predecessor)
+                # Attach the current node's right subtree to the `right of that predecessor`.
+                pred = curr.left
+                while pred.right:
+                    pred = pred.right
+                pred.right = curr.right
+
+                curr.right = curr.left
+                curr.left = None
+
+            curr = curr.right
